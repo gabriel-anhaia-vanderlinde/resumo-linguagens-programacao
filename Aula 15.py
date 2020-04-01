b7e37cf045f99ff149852cf97a94a93c8d214637
gabriel-anhaia-vanderlinde <61299524+gabriel-anhaia-vanderlinde@users.noreply.github.com>
2020-04-01 19:59:51 -0300

    Update Aula 15.py

--- Aula 15.py
+++ Aula 15.py
@@ -2,4 +2,4 @@ catetoad = float(input('Informe o cateto adjacente'))
 catetoop = float(input('Informe o cateto oposto'))
 hipotequ = catetoad*catetoad + catetoop*catetoop
 hipotefi = hipotequ**0.5
-print('A hipotenusa equivale a {}'.format(hipotefi)round)
+print('A hipotenusa equivale a {}'.format(hipotefi))

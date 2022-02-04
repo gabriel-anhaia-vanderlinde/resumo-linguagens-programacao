c8e2c7907e0449688105c9faf371a8d3ce84ff86
gabriel-anhaia-vanderlinde <61299524+gabriel-anhaia-vanderlinde@users.noreply.github.com>
2022-02-04 15:07:47 -0300

    Delete aula 19.py

--- aula 19.py
+++
@@ -1,8 +0,0 @@
-import random
-aluno1 = str(input('Nome do aluno'))
-aluno2 = str(input('Nome do aluno'))
-aluno3 = str(input('Nome do aluno'))
-aluno4 = str(input('Nome do aluno'))
-alunos = [aluno1, aluno2, aluno3, aluno4]
-random.shuffle(alunos)
-print('Sorteio:',alunos)

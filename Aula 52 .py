fc5f60e69e70bb3d2b8469e2cbdb8c9540fffd7e
gabriel-anhaia-vanderlinde <61299524+gabriel-anhaia-vanderlinde@users.noreply.github.com>
2022-02-04 15:19:31 -0300

    Delete Aula 52 .py

--- Aula 52 .py
+++
@@ -1,9 +0,0 @@
-
-palavras = ('caixa', 'veneno', 'bebida', 'carregador', 'diametro')
-
-for palavra in palavras:
-    print('A palavra {}, possui as segintes vogais: '.format(palavra), end='')
-    for letra in palavra:
-        if letra in 'aeiou':
-            print('{} ,'.format(letra), end='')
-    print()

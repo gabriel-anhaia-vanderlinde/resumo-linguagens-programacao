35e3b84fcf91e890ca8c9c85c3adf5741f5f1f85
gabriel-anhaia-vanderlinde <61299524+gabriel-anhaia-vanderlinde@users.noreply.github.com>
2022-02-04 15:19:19 -0300

    Delete Aula 51 .py

--- Aula 51 .py
+++
@@ -1,6 +0,0 @@
-lista = ('Feijão','5,00', 'Arroz', '3,99', 'Macarrão', '2,50')
-print('''
-o {} , está custando {}.
-o {} , está custando {}.
-o {} , está custando {}.
-'''.format(lista[0], lista[1], lista[2], lista[3], lista[4], lista[5]))

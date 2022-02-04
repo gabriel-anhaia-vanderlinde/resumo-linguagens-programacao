89770e62ed0fae1dddbe133ea0e6be65936bc4bf
gabriel-anhaia-vanderlinde <61299524+gabriel-anhaia-vanderlinde@users.noreply.github.com>
2022-02-04 15:02:29 -0300

    Delete Aula 7.py

--- Aula 7.py
+++
@@ -1,8 +0,0 @@
-nota1 = float(input('Primeira Nota'))
-nota2 = float(input('Segunda Nota'))
-media = (nota1+nota2)/2
-print('media do aluno igual a {}'.format(media))
-if(media >=7) :
-    print('Aprovado')
-else: 
-    print('Reprovado')

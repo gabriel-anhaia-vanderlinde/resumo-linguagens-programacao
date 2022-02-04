173c35338f6e96d4d717f61e7bcaea6772256f6a
gabriel-anhaia-vanderlinde <61299524+gabriel-anhaia-vanderlinde@users.noreply.github.com>
2022-02-04 15:06:00 -0300

    Resumo Python

--- Aula 9.py
+++ Aula 9.py
@@ -1,3 +1,55 @@
+valor = int(input('Quanto dinheiro você tem em R$?'))
+valorcv = valor / 4.39
+print('Em Dolar seu dinheiro de R${}, é equivalente a ${} hoje.'.format(valor, valorcv))
+/////////////////////////
+Largura = float(input('Informe a largura da parede'))
+Altura = float(input('Informe a altura da parede'))
+Litros = Largura*Altura / 2
+print('Uma parede com Largura {} e Altura {}, necessita de {} litros de tinta para pintar a parede'.format(Largura, Altura, Litros))
+/////////////////////////
+Valor = float(input('Valor do Produto?'))
+Desconto = Valor - Valor*(0.05)
+print('O produto custa R${} com desconto de 5% o pruduto custará R${}'.format(Valor, Desconto))
+/////////////////////////
+nota1 = float(input('Primeira Nota'))
+nota2 = float(input('Segunda Nota'))
+media = (nota1+nota2)/2
+print('media do aluno igual a {}'.format(media))
+if(media >=7) :
+    print('Aprovado')
+else: 
+    print('Reprovado')
+/////////////////////////
+nm1 = int(input('Escolha um número'))
+dobro = nm1 * 2
+triplo = nm1 * 3
+raiz = float(nm1) ** 0.5
+print('O número {} tem como dobro {}, como Triplo {}, e sua raiz é {}'.format(nm1, dobro, triplo, raiz ))
+/////////////////////////
+nm1 =int(input('Digite um numero'))
+nma = nm1 - 1
+nmd = nm1 + 1
+print('O numero {} tem com Antessesoro número {} e seu Sucessor como {}'.format(nm1, nma, nmd))
+/////////////////////////
+nome = input("Qual o seu nome? ")
+print('Seja bem vindo {}!.'.format (nome))
+/////////////////////////
+nm1 = int(input('DIgite um Número'))
+nm2 = int(input('Digite outro Número'))
+soma = nm1 + nm2
+print('soma de {} e {} é igual a {}'.format(nm1, nm2, soma))
+/////////////////////////
+nomeber = input('Digite um Numero')
+print('O tipo primitivo é', type(nomeber))
+print('Tem espaços?', nomeber.isspace())
+print('É um número?', nomeber.isnumeric())
+print('É alfabetico?', nomeber.isalpha())
+print('É alfanúmerico?', nomeber.isalnum())
+print('É maisculo?', nomeber.isupper())
+print('É minusculo?', nomeber.islower())
+print('É capitalizado?', nomeber.istitle())
+
+/////////////////////////
 Numero = float(input('Qual o número?'))
 print('O numero {}, multiplicado por {}, é igual a {}'.format(Numero, 0, Numero*0))
 print('O numero {}, multiplicado por {}, é igual a {}'.format(Numero, 1, Numero*1))
@@ -10,3 +62,33 @@ print('O numero {}, multiplicado por {}, é igual a {}'.format(Numero, 7, Numero
 print('O numero {}, multiplicado por {}, é igual a {}'.format(Numero, 8, Numero*8))
 print('O numero {}, multiplicado por {}, é igual a {}'.format(Numero, 9, Numero*9))
 print('O numero {}, multiplicado por {}, é igual a {}'.format(Numero, 10, Numero*10))
+
+/////////////////////////
+
+/////////////////////////
+
+/////////////////////////
+
+/////////////////////////
+
+/////////////////////////
+
+/////////////////////////
+
+/////////////////////////
+
+/////////////////////////
+
+/////////////////////////
+
+/////////////////////////
+
+/////////////////////////
+
+/////////////////////////
+
+/////////////////////////
+
+/////////////////////////
+
+/////////////////////////

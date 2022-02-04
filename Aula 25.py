4a07c3261d595cf91df7a8c18c33900a74197710
gabriel-anhaia-vanderlinde <61299524+gabriel-anhaia-vanderlinde@users.noreply.github.com>
2022-02-04 15:11:58 -0300

    Delete Aula 25.py

--- Aula 25.py
+++
@@ -1,5 +0,0 @@
-N1 = int(input('Digite Um numero: '))
-N2 = int(input('Digite Um numero: '))
-N3 = int(input('Digite Um numero: '))
-print('o maior numero é',max(N1, N2, N3))
-print('o menor numero é',min(N1, N2, N3))

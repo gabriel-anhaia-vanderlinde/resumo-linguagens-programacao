4c0922abd39123f75ec3df9a9b38d807ade16511
gabriel-anhaia-vanderlinde <61299524+gabriel-anhaia-vanderlinde@users.noreply.github.com>
2022-02-04 15:08:37 -0300

    Delete Aula 21.py

--- Aula 21.py
+++
@@ -1,8 +0,0 @@
-import random
-Numero = [0,1,2,3,4,5]
-Sortiado = random.choice(Numero)
-Escolha = int(input('Escolha um numero de 0 a 5 '))
-if Sortiado == Escolha:
-    print('Acertou miseravi!')
-else:
-    print('se fodeu')

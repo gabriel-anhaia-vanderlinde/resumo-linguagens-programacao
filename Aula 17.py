6997041bff4aac0163c690d3ace7bb578e64e55a
gabriel-anhaia-vanderlinde <61299524+gabriel-anhaia-vanderlinde@users.noreply.github.com>
2022-02-04 15:08:31 -0300

    Delete Aula 17.py

--- Aula 17.py
+++
@@ -1,7 +0,0 @@
-import random
-aluno1 = str(input('Nome do aluno'))
-aluno2 = str(input('Nome do aluno'))
-aluno3 = str(input('Nome do aluno'))
-aluno4 = str(input('Nome do aluno'))
-ganhador =[aluno1, aluno2, aluno3, aluno4]
-print('O ganhador foi {}'.format(random.choice(ganhador)))

eff4ec4b05c144dbcab7ebbb1c94adc2d04d98b5
gabriel-anhaia-vanderlinde <61299524+gabriel-anhaia-vanderlinde@users.noreply.github.com>
2022-02-04 15:02:48 -0300

    Delete Aula 6.py

--- Aula 6.py
+++
@@ -1,5 +0,0 @@
-nm1 = int(input('Escolha um número'))
-dobro = nm1 * 2
-triplo = nm1 * 3
-raiz = float(nm1) ** 0.5
-print('O número {} tem como dobro {}, como Triplo {}, e sua raiz é {}'.format(nm1, dobro, triplo, raiz ))

d1fd4ffc0e566584e1693e3a0e3dc8ef27c1a572
gabriel-anhaia-vanderlinde <61299524+gabriel-anhaia-vanderlinde@users.noreply.github.com>
2022-02-04 15:12:12 -0300

    Delete Aula 26.py

--- Aula 26.py
+++
@@ -1,7 +0,0 @@
-salario = float(input('indique o salário'))
-aum1251 = salario *0.10
-aum1250 = salario *0.15
-if salario > 1250:
-    print('O aumento do salário foi de R${}. '.format(aum1251))
-else:
-    print('O aumento de salário foi de R${}. '.format(aum1250))

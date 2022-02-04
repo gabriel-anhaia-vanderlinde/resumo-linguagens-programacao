2c2768261ffcdb76c87cd71680a3e117ec68a951
gabriel-anhaia-vanderlinde <61299524+gabriel-anhaia-vanderlinde@users.noreply.github.com>
2022-02-04 15:53:39 -0300

    Update and rename Aula exe pf 1.py to Desafio-Calculo.py

---
+++ Desafio-Calculo.py
@@ -0,0 +1,29 @@
+while not desligar:
+    Operacao = int(input("""Escolha qual operação matemática gostaria de fazer :
+       [1] Soma
+       [2] Subtração
+       [3] Divisão
+       [4] Multiplicação
+       [5] Exponenciação (Segundo número equivale a Exponeciação)
+       [6] Radiciação (Segundo número equivale a Radiciação)
+       [7] Resto de divisão
+          """)
+     num1 = int(input('Digite o primeiro número'))
+     num2 = int(input('Digite o Segundo número'))
+    
+    if Operacao = 1:
+        resultado := num1 + num2
+    elif Operacao == 2:
+        resultado := num1 - num2
+    elif Operacao == 3:
+        resultado := num1 / num2            
+    elif Operacao == 4:
+        resultado := num1 * num2     
+    elif Operacao == 5:
+        resultado := num1 ** num2            
+    elif Operacao == 6:
+        resultado := num1 * num2      
+    elif Operacao == 7:
+        resultado := num1 % num2      
+
+

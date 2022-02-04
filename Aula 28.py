b2d2c0df7345b6b78c090d12ebe03919e3d2c648
gabriel-anhaia-vanderlinde <61299524+gabriel-anhaia-vanderlinde@users.noreply.github.com>
2022-02-04 15:12:39 -0300

    Delete Aula 28.py

--- Aula 28.py
+++
@@ -1,9 +0,0 @@
-salario = float(input('Informe o salário: '))
-valor = float(input('Informe o valor da casa: '))
-anos = 12 * int(input('Informe em quantos anos íra pagar: '))
-salario30 = salario *0.30
-tempo =  anos / valor
-if salario30 >= tempo:
-    print('Seu emprestimo foi aprovado, Parabéns.')
-else:
-    print('Seu emprestimo foi reprovado.')

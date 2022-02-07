ead5e4a4da5b9d2e693362ae69d784b0b2b6edc6
gabriel-anhaia-vanderlinde <61299524+gabriel-anhaia-vanderlinde@users.noreply.github.com>
2022-02-07 13:22:23 -0300

    Update Dicionarios .py

--- Dicionarios .py
+++ Dicionarios .py
@@ -120,3 +120,19 @@ print(f'A média de idade é: {media} ')
 print(f'A lista de mulheres cadastrados foi de {len(mulheres)}, são as seguintes: ')
 for mulher in mulheres:
     print(f' Nome: {mulher.get("nome")}, idade: {mulher.get("idade")}')
+
+################################################ 
+
+matriz = []
+lista_filha = []
+print('Registrando valores em uma matriz: ')
+for i in range(0, 3):
+    print('Registrando valores a linha {}'.format(i))
+    for j in range(0, 3):
+        print('Registrando valores a coluna {}'.format(j))
+        lista_filha.append(input('Informe um valor para a linha {} e coluna {}:'.format(i, j)))
+
+        matriz.append(lista_filha)
+        lista_filha.clear
+
+print(matriz)
